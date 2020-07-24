5c702ecccff9d138d807765c5b234af4e2f4b6cd
Ugyen Norbu <unorbu2019@gmail.com>
2020-07-24 13:45:16 +0600

    initial visualization

--- script.R
+++ script.R
@@ -1,1 +1,14 @@
 library(tidyverse)
+library(ggplot2)
+library(lubridate)
+
+data <- read_csv("fatal-police-shootings-data.csv")
+data <- data %>% 
+    select(-id)
+str(data)
+
+data %>% 
+    group_by(month_year=floor_date(date, "month")) %>% 
+    tally() %>% 
+    ggplot(aes(x = month_year, y = n)) +
+    geom_line()
